c07c0d9dc08ba3f4cd0684a8887156cb8f493960
Genn Cai <caigeng0204@126.com>
2018-09-30 11:12:10 +0800

    add precss

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import styles from './App.scss';
+
+
+class App extends Component {
+  render() {
+    console.log('styles', styles);
+    
+    return (
+      <div className={styles['App']}>
+        <header className={styles['App-header']}>
+          <img src={logo} className={styles["App-logo"]} alt="logo" />
+          <h1 className={styles["App-title"]}>Welcome to React</h1>
+        </header>
+        <p className={styles["App-intro"]}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
